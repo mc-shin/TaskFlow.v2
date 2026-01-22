093455ffd258839b19227a89abc8b330fe3f5d69
mc-shin <mcshinqb@gmail.com>
2026-01-22 17:50:17 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -75,8 +75,8 @@ export default defineConfig({
 
     //////
     // outDir: resolve(__dirname, "dist"),
-    // outDir: "dist",
-    outDir: "../dist",
+    outDir: "dist",
+    // outDir: "../dist",
     emptyOutDir: true,
     ///////
   },
